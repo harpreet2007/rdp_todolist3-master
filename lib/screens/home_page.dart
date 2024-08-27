b120fe45b912129bdd2a82fa3ef9f5bc826de6be
harpreet2007 <2003harpreetkaur02@gmail.com>
2024-08-27 13:38:14 -0600

    my commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  final List<String> tasks = <String>[];
+  final List<bool> checkboxes = List.generate(8, (index) => false);
+  TextEditingController nameController = TextEditingController();
+
+  bool isChecked = false;
+
+  void addItemToList() async {
+    final String taskName = nameController.text;
+
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': false,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+
+    setState(() {
+      tasks.insert(0, taskName);
+    });
+  }
+
+  void removeItems(int index) async {
+    //Get the tasks to be removed
+    String taskToBeRemoved = tasks[index];
+
+    //Remove the task from Firestore
+    QuerySnapshot querySnapshot = await db
+        .collection('tasks')
+        .where('name', isEqualTo: taskToBeRemoved)
+        .get();
+
+    if (querySnapshot.size > 0) {
+      DocumentSnapshot documentSnapshot = querySnapshot.docs[0];
+
+      await documentSnapshot.reference.delete();
+    }
+
+    setState(() {
+      tasks.removeAt(index);
+      checkboxes.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [SizedBox(
+            height: 80,
+            child: Image.asset('assets/rdplogo.png'),
+          ),
+          const Text(
+            'Dialy Planner'
+            style: TextStyle(
+              fontFamily: 'cavert' , fontSize: 32 , color:Colors.white),
+            ),
+
+         
+          ],
+        ),
+      ),
+    );
+  }
+}
